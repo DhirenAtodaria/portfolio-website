c846b60d684cc43225fc768ec8e7736aae33589f
DhirenAtodaria <dhiren.atodaria@hotmail.co.uk>
2020-01-26 15:12:19 +0000

    added scrollmagic

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const instance = new TypeIt('#replaceStrings', {
+const instance = new TypeIt('.replaceStrings', {
     speed: 100,
     waitUntilVisible: true
   })
@@ -11,3 +11,44 @@ const instance = new TypeIt('#replaceStrings', {
   .type('Scroll to see what I\'ve done...')
   .pause(750)
   .go();
+
+
+const tween = new TimelineLite();
+
+tween.add(
+    TweenLite.to('#anim1', 10, {opacity: 0
+    })
+);
+
+
+const controller = new ScrollMagic.Controller();
+
+const scene = new ScrollMagic.Scene({
+    triggerElement:'.one',
+    duration: '100%',
+    triggerHook: 0
+})
+
+.setPin('.one')
+.setTween(tween)
+.addTo(controller);
+
+const tween2 = new TimelineLite();
+
+tween2.add(
+    TweenLite.to('#reveal1', 20, {opacity: 1
+    })
+);
+
+tween2.add(
+    TweenLite.to('#reveal1', 20, {opacity: 0})
+);
+
+const scene1 = new ScrollMagic.Scene({
+    triggerElement:'.two',
+    duration: '100%',
+    triggerHook: 0
+})
+.setPin('.two')
+.setTween(tween2)
+.addTo(controller);
