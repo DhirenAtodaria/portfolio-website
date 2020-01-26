94f4c388ec93579347b9eb2f16269e1289259e69
DhirenAtodaria <dhiren.atodaria@hotmail.co.uk>
2020-01-26 21:00:04 +0000

    added backgrounds

--- sandbox.js
+++ sandbox.js
@@ -2,10 +2,10 @@ const instance = new TypeIt('.replaceStrings', {
     speed: 100,
     waitUntilVisible: true
   })
-  .type('My name is Dhiren Atodaria.')
+  .type('My name is <b>Dhiren Atodaria</b>.')
   .pause(1000)
   .delete(27)
-  .type('and I am a Front End Developer')
+  .type('and I am a Web Developer.')
   .pause(1000)
   .delete(30)
   .type('Scroll to see what I\'ve done...')
@@ -16,7 +16,7 @@ const instance = new TypeIt('.replaceStrings', {
 const tween = new TimelineLite();
 
 tween.add(
-    TweenLite.to('#anim1', 10, {opacity: 0
+    TweenLite.to('#anim1', 20, {opacity: 0
     })
 );
 
@@ -36,12 +36,12 @@ const scene = new ScrollMagic.Scene({
 const tween2 = new TimelineLite();
 
 tween2.add(
-    TweenLite.to('#reveal1', 20, {opacity: 1
+    TweenLite.to('#bg, #reveal1', 30, {opacity: 1
     })
 );
 
 tween2.add(
-    TweenLite.to('#reveal1', 20, {opacity: 0})
+    TweenLite.to('#bg, #reveal1', 30, {opacity: 0})
 );
 
 const scene1 = new ScrollMagic.Scene({
@@ -53,29 +53,59 @@ const scene1 = new ScrollMagic.Scene({
 .setTween(tween2)
 .addTo(controller);
 
-
-TweenLite.set('#reveal2',{x:'-101%'})
-TweenLite.set('#anim2',{x:'-101%'})
-TweenLite.set('#anim3', {x:'+101%'})
-TweenLite.set('#anim4',{x:'-101%'})
-
 const tween3 = new TimelineLite();
 
 tween3.add(
-    TweenLite.to('#reveal2',2,{x:'0%',ease: Linear.easeNone})
+    TweenLite.to('#reveal2', 1, {opacity: 1})
 );
 
 tween3.add(
-    TweenLite.to('#anim2',2,{x:'0%',ease: Linear.easeNone})
+    TweenLite.to('#anim2', 1, {opacity: 1})
 )
 
 tween3.add(
-    TweenLite.to('#anim3',2,{x:'0%',ease: Linear.easeNone})
+    TweenLite.to('#anim3', 1, {opacity: 1})
 )
 
 tween3.add(
-    TweenLite.to('#anim4',2,{x:'0%',ease: Linear.easeNone})
+    TweenLite.to('#anim4', 1, {opacity: 1})
 )
+
+tween3.add(
+    TweenLite.to('#anim5', 1, {opacity: 1})
+)
+
+const scene2 = new ScrollMagic.Scene({
+    triggerElement:'.three',
+    duration: '100%',
+    
+})
+
+.setTween(tween3)
+.addTo(controller);
+
+const tween4 = new TimelineLite();
+
+tween4.add(
+    TweenLite.to('#reveal3, #bg2', 30, {opacity: 1
+    })
+);
+
+tween4.add(
+    TweenLite.to('#reveal3, #bg2', 30, {opacity: 0})
+);
+
+const scene3 = new ScrollMagic.Scene({
+    triggerElement:'.four',
+    duration: '100%',
+    triggerHook: 0
+})
+
+.setPin('.four')
+.setTween(tween4)
+.addTo(controller);
+
+
 
 
 
