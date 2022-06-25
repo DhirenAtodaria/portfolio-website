035c70644a40375f5aafc381ac624693a5ea5b95
DhirenAtodaria <dhiren.atodaria@hotmail.co.uk>
2022-06-25 21:35:37 +0100

    fixed loading

--- src/Loader.js
+++ src/Loader.js
@@ -2,10 +2,16 @@ import React, { useEffect, useRef } from "react";
 import { gsap, Power1, Expo } from "gsap";
 import { css } from "@emotion/css";
 import { motion } from "framer-motion";
+import { useProgress } from "@react-three/drei";
 
 export const Loader = ({ loading, setLoading }) => {
     const loadingRef = useRef();
+    const spinnerRef = useRef();
+    const loadingFinishedRef = useRef([]);
     const screenRef = useRef();
+    const allowAnimation = useRef(false);
+
+    const { progress } = useProgress();
 
     useEffect(() => {
         const loadingAnimation = gsap.timeline();
@@ -15,6 +21,33 @@ export const Loader = ({ loading, setLoading }) => {
             { opacity: 0.9, duration: 3, delay: 0.5, ease: Power1.easeOut }
         );
     }, []);
+
+    useEffect(() => {
+        if (progress === 100) {
+            const loadingFinishedAnimation = gsap.timeline();
+            loadingFinishedAnimation
+                .to(spinnerRef.current, {
+                    opacity: 0,
+                    duration: 1,
+                    delay: 2,
+                    ease: Power1.easeOut,
+                    stagger: { each: 0.5 },
+                })
+                .to(
+                    loadingFinishedRef.current,
+                    {
+                        y: "0%",
+                        duration: 0.75,
+                        ease: Power1.easeOut,
+                        stagger: { each: 0.5 },
+                        onComplete: () => {
+                            allowAnimation.current = true;
+                        },
+                    },
+                    "-=0.25"
+                );
+        }
+    }, [progress]);
 
     useEffect(() => {
         if (!loading) {
@@ -41,7 +74,11 @@ export const Loader = ({ loading, setLoading }) => {
                 background-color: black;
             `}
             ref={screenRef}
-            onClick={() => setLoading(false)}
+            onClick={() => {
+                if (allowAnimation.current) {
+                    setLoading(false);
+                }
+            }}
         >
             <div
                 ref={loadingRef}
@@ -63,7 +100,12 @@ export const Loader = ({ loading, setLoading }) => {
                         direction: rtl;
                     `}
                 >
-                    <svg width="24px" height="24px" viewBox="0 0 24 24">
+                    <svg
+                        ref={spinnerRef}
+                        width="24px"
+                        height="24px"
+                        viewBox="0 0 24 24"
+                    >
                         <motion.path
                             d="M 2, 12 a 10, 10 0 1,0 20,0 a 10, 10 0 1,0 -20,0"
                             strokeWidth={1}
@@ -85,12 +127,38 @@ export const Loader = ({ loading, setLoading }) => {
                     </svg>
                     <div
                         className={css`
-                            color: white;
+                            overflow: hidden;
+                            height: 14px;
+                            width: 100%;
                             margin-top: 1.5em;
-                            font-size: 0.6em;
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
                         `}
                     >
-                        LOADING
+                        <div
+                            ref={(e) => (loadingFinishedRef.current[1] = e)}
+                            className={css`
+                                color: white;
+                                font-size: 12px;
+                                margin-right: 0.5em;
+                                transform: translate(0px, -100%);
+                                cursor: pointer;
+                            `}
+                        >
+                            ENTER
+                        </div>
+                        <div
+                            ref={(e) => (loadingFinishedRef.current[0] = e)}
+                            className={css`
+                                color: white;
+                                font-size: 12px;
+                                margin-right: 0.5em;
+                                transform: translate(0px, -100%);
+                            `}
+                        >
+                            LOADING
+                        </div>
                     </div>
                 </div>
             </div>
